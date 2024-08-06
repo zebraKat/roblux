b773dc792e30fb9261e88c2c6e71fc90c69fa7da
ZebraKat <hasselbush_kids3@icloud.com>
2024-08-06 19:56:30 +0000

    Update Source.lua

--- GitFlash/Source.lua
+++ GitFlash/Source.lua
@@ -22,7 +22,8 @@ type OptionalThemeData = {
 	InactivetabColor:Color3,
 	ActivetabColor:Color3,
 	TitleColor:Color3,
-	BigButtonColor:Color3
+	BigButtonColor:Color3,
+	BigButtonPrimaryColor:Color3
 }
 
 function ValidateTheme(inputTheme)
@@ -35,7 +36,8 @@ function ValidateTheme(inputTheme)
 		InactivetabColor=RGB(104, 108, 116),
 		ActivetabColor = RGB(0, 0, 0),
 		TitleColor = RGB(0, 0, 0),
-		BigButtonColor = RGB(40,116,236)
+		BigButtonColor = RGB(40,116,236),
+		BigButtonPrimaryColor=RGB(36, 105, 209)
 	}
 
 	for i,v in Colors do
@@ -52,7 +54,7 @@ function GF:Window(Title:string,OptionalCloseKey:Enum.KeyCode?,OptionalThemeData
 		error("GitFlashLib: CHOOSE A NAME ")
 	end
 
-	
+
 	local GUI = {
 		selectedTab = nil,
 	}
@@ -65,7 +67,7 @@ function GF:Window(Title:string,OptionalCloseKey:Enum.KeyCode?,OptionalThemeData
 	--	ActivetabColor = RGB(0, 0, 0),
 	--	TitleColor = RGB(0, 0, 0)
 	--}
-	
+
 	--local Colors:OptionalThemeData = { github UI dark mode
 	--	MainColor=RGB(13,17,23),
 	--	SecondaryColor=RGB(1,4,9),
@@ -75,11 +77,11 @@ function GF:Window(Title:string,OptionalCloseKey:Enum.KeyCode?,OptionalThemeData
 	--	ActivetabColor = RGB(213,220,226),
 	--	TitleColor = RGB(219,226,232)
 	--}
-	
+
 	function GUI:Destroy()
 		GUI["1"]:Destroy()
 	end
-	
+
 	local Colors = ValidateTheme(OptionalThemeData or {})
 
 	GUI["1"] = Instance.new("ScreenGui")
@@ -168,8 +170,8 @@ function GF:Window(Title:string,OptionalCloseKey:Enum.KeyCode?,OptionalThemeData
 		GUI["6"]["Name"] = [[CloseButton]];
 		GUI["6"]["Position"] = UDim2.new(0.99914, 0, 0.42857, 0);
 		GUI["6Hover"] = false
-		 
-		
+
+
 		GUI["6"].MouseEnter:Connect(function()
 			GUI["6Hover"] = true
 			GUI["6"].TextColor3 = Colors.ActivetabColor
@@ -178,10 +180,10 @@ function GF:Window(Title:string,OptionalCloseKey:Enum.KeyCode?,OptionalThemeData
 			GUI["6Hover"] = false
 			GUI["6"].TextColor3 = Colors.InactivetabColor
 		end)
-		
+
 		inputser.InputBegan:Connect(function(inp,gpe)
 			if gpe then return end
-			if inp.UserInputType == (Enum.UserInputType.MouseButton1 or Enum.UserInputType.Touch) then
+			if inp.UserInputType == Enum.UserInputType.MouseButton1 or inp.UserInputType== Enum.UserInputType.Touch then
 				if GUI["6Hover"] then
 					GUI:Destroy()
 				end
@@ -279,7 +281,7 @@ function GF:Window(Title:string,OptionalCloseKey:Enum.KeyCode?,OptionalThemeData
 		GUI["13"]["Name"] = [[ScreenArea]];
 		GUI["13"]["BackgroundTransparency"] = 1;
 		GUI["13"]["ClipsDescendants"] = true
-		
+
 		-- StarterGui.ScreenGui.Window.ScreenArea.UICorner
 		GUI["14"] = Instance.new("UICorner", GUI["13"]);
 		GUI["14"]["CornerRadius"] = UDim.new(0, 3);
@@ -310,6 +312,18 @@ function GF:Window(Title:string,OptionalCloseKey:Enum.KeyCode?,OptionalThemeData
 		tab["1"]["ScrollBarThickness"] = 3;
 		tab["1"]["BackgroundTransparency"] = 1;
 		tab["1"].Visible = false
+		-- StarterGui.ScreenGui.UIListLayout
+		tab["1l"] = Instance.new("UIListLayout", tab["1"]);
+		tab["1l"]["HorizontalAlignment"] = Enum.HorizontalAlignment.Center;
+		tab["1l"]["Padding"] = UDim.new(0, 8);
+		tab["1l"]["SortOrder"] = Enum.SortOrder.LayoutOrder;
+
+		-- StarterGui.ScreenGui.UIPadding
+		tab["1p"] = Instance.new("UIPadding", tab["1"]);
+		tab["1p"]["PaddingTop"] = UDim.new(0, 8);
+		tab["1p"]["PaddingRight"] = UDim.new(0, 4);
+		tab["1p"]["PaddingLeft"] = UDim.new(0, -8);
+		tab["1p"]["PaddingBottom"] = UDim.new(0, 4);
 
 		-- StarterGui.ScreenGui.Inactivetab
 		tab["2"] = Instance.new("TextLabel", GUI["10"]);
@@ -345,8 +359,8 @@ function GF:Window(Title:string,OptionalCloseKey:Enum.KeyCode?,OptionalThemeData
 		-- StarterGui.ScreenGui.Inactivetab.UIPadding
 		tab["5"] = Instance.new("UIPadding", tab["2"]);
 		tab["5"]["PaddingLeft"] = UDim.new(0, 8);
-		
-		
+
+
 		function tab:Select()
 			if tab._state == "inactive" then
 				tab._state = "active"
@@ -366,7 +380,7 @@ function GF:Window(Title:string,OptionalCloseKey:Enum.KeyCode?,OptionalThemeData
 			tab["2"].TextColor3 = Colors.InactivetabColor
 			tab["3"].ImageColor3 = Colors.InactivetabColor
 		end
-		
+
 
 		tab["2"].MouseEnter:Connect(function()
 			tab['2'].Text= `<u> {Name} </u>`
@@ -385,14 +399,284 @@ function GF:Window(Title:string,OptionalCloseKey:Enum.KeyCode?,OptionalThemeData
 
 		inputser.InputBegan:Connect(function(inp,gpe)
 			if gpe then return end
-			if inp.UserInputType == (Enum.UserInputType.MouseButton1 or Enum.UserInputType.Touch) then
+			if inp.UserInputType == Enum.UserInputType.MouseButton1 or inp.UserInputType == Enum.UserInputType.Touch then
 				if tab._hover then
 					tab:Select()
 				end
 			end
 		end)
-		
-		
+
+		function tab:Image(imgID:string)
+			local image = {}
+			-- StarterGui.ScreenGui.Image
+			image["4"] = Instance.new("Frame", tab["1"]);
+			image["4"]["BorderSizePixel"] = 0;
+			image["4"]["BackgroundColor3"] = Color3.fromRGB(34, 39, 46);
+			image["4"]["Size"] = UDim2.new(0.95, 0, 0.26272, 45);
+			image["4"]["Position"] = UDim2.new(0.025, 0, 0.31558, 0);
+			image["4"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+			image["4"]["Name"] = [[Image]];
+			image["4"]["BackgroundTransparency"] = 1;
+
+			-- StarterGui.ScreenGui.Image.UICorner
+			image["5"] = Instance.new("UICorner", image["4"]);
+
+
+			-- StarterGui.ScreenGui.Image.Text Label
+			image["6"] = Instance.new("ImageLabel", image["4"]);
+			image["6"]["BorderSizePixel"] = 0;
+			image["6"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+			image["6"]["ScaleType"] = Enum.ScaleType.Fit;
+			image["6"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+			image["6"]["Image"] = imgID;
+			image["6"]["Size"] = UDim2.new(0, 200, 0, 111);
+			image["6"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+			image["6"]["BackgroundTransparency"] = 1;
+			image["6"]["Name"] = [[Text Label]];
+			image["6"]["Position"] = UDim2.new(0.5, 0, 0.50453, 0);
+			return image
+		end
+		function tab:ImageButton(imgID:string,label:string,callback:() -> ())
+			
+			local imgButton = {}
+			
+			-- StarterGui.ScreenGui.ImageButton
+			imgButton["7"] = Instance.new("Frame", tab["1"]);
+			imgButton["7"]["BorderSizePixel"] = 0;
+			imgButton["7"]["BackgroundColor3"] = Colors.BigButtonPrimaryColor;
+			imgButton["7"]["Size"] = UDim2.new(0.95, 0, 0, 45);
+			imgButton["7"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+			imgButton["7"]["Name"] = [[ImageButton]];
+
+			-- StarterGui.ScreenGui.ImageButton.UICorner
+			imgButton["8"] = Instance.new("UICorner", imgButton["7"]);
+
+
+			-- StarterGui.ScreenGui.ImageButton.UIStroke
+			imgButton["9"] = Instance.new("UIStroke", imgButton["7"]);
+			imgButton["9"]["Color"] = Color3.new(Colors.BigButtonPrimaryColor.R + 0.225,Colors.BigButtonPrimaryColor.G + 0.225, Colors.BigButtonPrimaryColor.B + 0.225);
+			
+			
+			-- StarterGui.ScreenGui.ImageButton.TextLabel
+			imgButton["a"] = Instance.new("TextLabel", imgButton["7"]);
+			imgButton["a"]["BorderSizePixel"] = 0;
+			imgButton["a"]["TextXAlignment"] = Enum.TextXAlignment.Left;
+			imgButton["a"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+			imgButton["a"]["TextSize"] = 18;
+			imgButton["a"]["FontFace"] = Font.new([[rbxassetid://12187370747]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+			imgButton["a"]["TextColor3"] = Color3.fromRGB(254, 254, 254);
+			imgButton["a"]["BackgroundTransparency"] = 1;
+			imgButton["a"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+			imgButton["a"]["Size"] = UDim2.new(0, 96, 0, 50);
+			imgButton["a"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+			imgButton["a"]["Text"] = label;
+			imgButton["a"]["Position"] = UDim2.new(0.55, 0, 0.5, 0);
+
+			-- StarterGui.ScreenGui.ImageButton.TextLabel.icon
+			imgButton["b"] = Instance.new("ImageLabel", imgButton["a"]);
+			imgButton["b"]["BorderSizePixel"] = 0;
+			imgButton["b"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+			imgButton["b"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+			imgButton["b"]["Image"] = imgID;
+			imgButton["b"]["Size"] = UDim2.new(0, 30, 0, 30);
+			imgButton["b"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+			imgButton["b"]["BackgroundTransparency"] = 1;
+			imgButton["b"]["Name"] = [[icon]];
+			imgButton["b"]["Position"] = UDim2.new(-0.22489, 0, 0.5, 0);
+			
+			imgButton._hovered = false
+			
+			imgButton["7"].MouseEnter:Connect(function()
+				imgButton._hovered = true
+				imgButton['7'].BackgroundColor3 = Color3.new(Colors.BigButtonPrimaryColor.R + 0.05,Colors.BigButtonPrimaryColor.G + 0.05, Colors.BigButtonPrimaryColor.B + 0.05)
+			end)
+			imgButton["7"].MouseLeave:Connect(function()
+				imgButton._hovered = false
+				imgButton["7"]["BackgroundColor3"] = Colors.BigButtonPrimaryColor;
+			end)
+			
+			
+			inputser.InputBegan:Connect(function(inp,gpe)
+				if imgButton._hovered then
+					if inp.UserInputType == Enum.UserInputType.MouseButton1 or inp.UserInputType ==Enum.UserInputType.Touch then
+							callback()
+					end
+				end
+			end)
+			
+			return imgButton
+		end
+		function tab:Label(text:string)
+			local label = {}
+			-- StarterGui.ScreenGui.Label
+			label["c"] = Instance.new("Frame", tab["1"]);
+			label["c"]["BorderSizePixel"] = 0;
+			label["c"]["BackgroundColor3"] = Color3.fromRGB(34, 39, 46);
+			label["c"]["Size"] = UDim2.new(0.95, 0, 0, 45);
+			label["c"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+			label["c"]["Name"] = [[Label]];
+
+			-- StarterGui.ScreenGui.Label.UICorner
+			label["d"] = Instance.new("UICorner", label["c"]);
+
+
+			-- StarterGui.ScreenGui.Label.UIStroke
+			label["e"] = Instance.new("UIStroke", label["c"]);
+			label["e"]["Color"] = Color3.fromRGB(49, 56, 66);
+
+			-- StarterGui.ScreenGui.Label.Text Label
+			label["f"] = Instance.new("TextLabel", label["c"]);
+			label["f"]["BorderSizePixel"] = 0;
+			label["f"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+			label["f"]["TextSize"] = 18;
+			label["f"]["FontFace"] = Font.new([[rbxassetid://12187370747]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+			label["f"]["TextColor3"] = Color3.fromRGB(254, 254, 254);
+			label["f"]["BackgroundTransparency"] = 1;
+			label["f"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+			label["f"]["Size"] = UDim2.new(0, 200, 0, 50);
+			label["f"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+			label["f"]["Text"] = text;
+			label["f"]["Name"] = [[Text Label]];
+			label["f"]["Position"] = UDim2.new(0.5, 0, 0.5, 0);
+			return label
+		end
+		function tab:MultiButton()
+			local multibutton = {}
+
+			-- StarterGui.ScreenGui.MultiButton
+			multibutton["10"] = Instance.new("Frame", tab["1"]);
+			multibutton["10"]["BorderSizePixel"] = 0;
+			multibutton["10"]["BackgroundColor3"] = Color3.fromRGB(34, 39, 46);
+			multibutton["10"]["Size"] = UDim2.new(0.95, 0, 0, 45);
+			multibutton["10"]["Position"] = UDim2.new(0.025, 0, 0.67254, 0);
+			multibutton["10"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+			multibutton["10"]["Name"] = [[MultiButton]];
+			multibutton["10"]["BackgroundTransparency"] = 1;
+
+			-- StarterGui.ScreenGui.MultiButton.UICorner
+			multibutton["11"] = Instance.new("UICorner", multibutton["10"]);
+
+
+			-- StarterGui.ScreenGui.MultiButton.UIStroke
+			multibutton["12"] = Instance.new("UIStroke", multibutton["10"]);
+			multibutton["12"]["Color"] = Color3.fromRGB(49, 56, 66);
+
+			-- StarterGui.ScreenGui.MultiButton.UIListLayout
+			multibutton["13"] = Instance.new("UIListLayout", multibutton["10"]);
+			multibutton["13"]["HorizontalAlignment"] = Enum.HorizontalAlignment.Center;
+			multibutton["13"]["Padding"] = UDim.new(0, 8);
+			multibutton["13"]["VerticalAlignment"] = Enum.VerticalAlignment.Center;
+			multibutton["13"]["SortOrder"] = Enum.SortOrder.LayoutOrder;
+			multibutton["13"]["FillDirection"] = Enum.FillDirection.Horizontal;
+			
+			function multibutton:TextButton(text,callback)
+				local stextbutton = {}
+				-- StarterGui.ScreenGui.MultiButton.SmallTextButton
+				stextbutton["14"] = Instance.new("Frame", multibutton["10"]);
+				stextbutton["14"]["BorderSizePixel"] = 0;
+				stextbutton["14"]["BackgroundColor3"] =  Colors.BigButtonPrimaryColor;
+				stextbutton["14"]["Size"] = UDim2.new(0.30633, 0, 0.06943, 35);
+				stextbutton["14"]["Position"] = UDim2.new(0.10525, 0, 0.07472, 0);
+				stextbutton["14"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+				stextbutton["14"]["Name"] = [[SmallTextButton]];
+
+				-- StarterGui.ScreenGui.MultiButton.SmallTextButton.UICorner
+				stextbutton["15"] = Instance.new("UICorner", stextbutton["14"]);
+
+
+				-- StarterGui.ScreenGui.MultiButton.SmallTextButton.UIStroke
+				stextbutton["16"] = Instance.new("UIStroke", stextbutton["14"]);
+				stextbutton["16"]["Color"] = Color3.new(Colors.BigButtonPrimaryColor.R + 0.225,Colors.BigButtonPrimaryColor.G + 0.225, Colors.BigButtonPrimaryColor.B + 0.225);
+
+				-- StarterGui.ScreenGui.MultiButton.SmallTextButton.TextLabel
+				stextbutton["17"] = Instance.new("TextLabel", stextbutton["14"]);
+				stextbutton["17"]["BorderSizePixel"] = 0;
+				stextbutton["17"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+				stextbutton["17"]["TextSize"] = 18;
+				stextbutton["17"]["FontFace"] = Font.new([[rbxassetid://12187370747]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+				stextbutton["17"]["TextColor3"] = Color3.fromRGB(254, 254, 254);
+				stextbutton["17"]["BackgroundTransparency"] = 1;
+				stextbutton["17"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+				stextbutton["17"]["Size"] = UDim2.new(0, 49, 0, 50);
+				stextbutton["17"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+				stextbutton["17"]["Text"] = text;
+				stextbutton["17"]["Position"] = UDim2.new(0.49517, 0, 0.48322, 0);
+				
+				stextbutton._hovered = false
+				
+				stextbutton["14"].MouseEnter:Connect(function()
+					stextbutton._hovered = true
+					stextbutton['14'].BackgroundColor3 = Color3.new(Colors.BigButtonPrimaryColor.R + 0.05,Colors.BigButtonPrimaryColor.G + 0.05, Colors.BigButtonPrimaryColor.B + 0.05)
+				end)
+				stextbutton["14"].MouseLeave:Connect(function()
+					stextbutton._hovered = false
+					stextbutton["14"]["BackgroundColor3"] = Colors.BigButtonPrimaryColor;
+				end)
+				
+				inputser.InputBegan:Connect(function(inp,gpe)
+					if gpe then return end
+					
+					if inp.UserInputType == Enum.UserInputType.Touch or inp.UserInputType == Enum.UserInputType.MouseButton1 then
+						if stextbutton._hovered == true then
+							callback()
+						end
+					end
+				end)
+				
+				return stextbutton
+			end
+			
+			function multibutton:ImageButton(imageID,text,callback)
+				local imagebuttonM = {}
+				-- StarterGui.ScreenGui.MultiButton.SmallImageButton
+				imagebuttonM["18"] = Instance.new("Frame", imagebuttonM["10"]);
+				imagebuttonM["18"]["BorderSizePixel"] = 0;
+				imagebuttonM["18"]["BackgroundColor3"] = Color3.fromRGB(36, 105, 209);
+				imagebuttonM["18"]["Size"] = UDim2.new(0.306, 0, 0.069, 35);
+				imagebuttonM["18"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+				imagebuttonM["18"]["Name"] = [[SmallImageButton]];
+
+				-- StarterGui.ScreenGui.MultiButton.SmallImageButton.UICorner
+				imagebuttonM["19"] = Instance.new("UICorner", imagebuttonM["18"]);
+
+
+				-- StarterGui.ScreenGui.MultiButton.SmallImageButton.UIStroke
+				imagebuttonM["1a"] = Instance.new("UIStroke", imagebuttonM["18"]);
+				imagebuttonM["1a"]["Color"] = Color3.fromRGB(65, 141, 255);
+
+				-- StarterGui.ScreenGui.MultiButton.SmallImageButton.TextLabel
+				imagebuttonM["1b"] = Instance.new("TextLabel", imagebuttonM["18"]);
+				imagebuttonM["1b"]["TextWrapped"] = true;
+				imagebuttonM["1b"]["BorderSizePixel"] = 0;
+				imagebuttonM["1b"]["TextXAlignment"] = Enum.TextXAlignment.Left;
+				imagebuttonM["1b"]["TextScaled"] = true;
+				imagebuttonM["1b"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+				imagebuttonM["1b"]["TextSize"] = 17;
+				imagebuttonM["1b"]["FontFace"] = Font.new([[rbxassetid://12187370747]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+				imagebuttonM["1b"]["TextColor3"] = Color3.fromRGB(254, 254, 254);
+				imagebuttonM["1b"]["BackgroundTransparency"] = 1;
+				imagebuttonM["1b"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+				imagebuttonM["1b"]["Size"] = UDim2.new(0, 68, 0, 38);
+				imagebuttonM["1b"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+				imagebuttonM["1b"]["Text"] = text;
+				imagebuttonM["1b"]["Position"] = UDim2.new(0.77463, 0, 0.49953, 0);
+
+				-- StarterGui.ScreenGui.MultiButton.SmallImageButton.TextLabel.icon
+				imagebuttonM["1c"] = Instance.new("ImageLabel", imagebuttonM["1b"]);
+				imagebuttonM["1c"]["BorderSizePixel"] = 0;
+				imagebuttonM["1c"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+				imagebuttonM["1c"]["AnchorPoint"] = Vector2.new(0.5, 0.5);
+				imagebuttonM["1c"]["Image"] = imageID;
+				imagebuttonM["1c"]["Size"] = UDim2.new(0, 30, 0, 30);
+				imagebuttonM["1c"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+				imagebuttonM["1c"]["BackgroundTransparency"] = 1;
+				imagebuttonM["1c"]["Name"] = [[icon]];
+				imagebuttonM["1c"]["Position"] = UDim2.new(-0.35822, 0, 0.5, 0);
+			end
+			
+			return multibutton
+		end
+		return tab
 
 	end
 
