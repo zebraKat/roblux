dd8a527e2e1aa0565bbe10e7af9e5af003999fb9
ZebraKat <hasselbush_kids3@icloud.com>
2024-08-10 05:51:22 +0000

    Update EasyDecomp.lua

--- EasyDecomp.lua
+++ EasyDecomp.lua
@@ -9,10 +9,10 @@ function x.decomp(modulePATH)
 		for b,c in a do
 			if typeof(c) == "table" then
 				for d,e in c do
-					retString = retString..`\n {c}.{d} = {e}`
+					retString = retString..`\n {c}.{d} = {e} *{typeof(e)}`
 				end
 			else
-				retString = retString.. `\n {a}.{b} = {c}`
+				retString = retString.. `\n {a}.{b} = {c} *{typeof(c)}`
 			end
 		end
 		return retString..`}`
@@ -22,7 +22,7 @@ function x.decomp(modulePATH)
 		if typeof(v) == "table" then
 			GoTable(v)
 		else
-			returnstring = returnstring.. `\n module.{i} = {v}`
+			returnstring = returnstring.. `\n module.{i} = {v} *{typeof(v)}`
 		end
 	end
 	
