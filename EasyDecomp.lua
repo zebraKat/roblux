4e895ed1a5fc8c50b80dd841ffd10e29c015ff19
ZebraKat <hasselbush_kids3@icloud.com>
2024-08-12 18:54:03 +0000

    FORMATTED

--- EasyDecomp.lua
+++ EasyDecomp.lua
@@ -1,27 +1,29 @@
 local ezDecomp = {}
 
-print "Running EzDecomp V2 (Module Scripts only Currently)"
+print("Running EzDecomp V2 (Module Scripts only Currently)")
 
 function ezDecomp.decomp(mod)
 	assert(mod:IsA("ModuleScript"))
 
-	local req 
+	local req
 
 	local success, err = pcall(function()
 		req = require(mod)
 	end)
 
-	if success then print("EzDecomp successfully decompiled the ModuleScript!") else 
-		error("Easy Decomp: "..err)
+	if success then
+		print("EzDecomp successfully decompiled the ModuleScript!")
+	else
+		error("Easy Decomp: " .. err)
 	end
 
-	local returnString = mod.Name.." = {}"
+	local returnString = mod.Name .. " = {}"
 
 	local function AddToReturn(addon)
-		returnString = returnString..`\n{addon}`
+		returnString = returnString .. `\n{addon}`
 	end
 
-	local function DecompMain(value,name):string
+	local function DecompMain(value, name): string
 		if type(value) == "string" then
 			return `{name}:{typeof(value)} = "{value}"`
 		else
@@ -29,34 +31,33 @@ function ezDecomp.decomp(mod)
 		end
 	end
 
-	local function DecompTable(table,name)
+	local function DecompTable(table, name)
 		local ret = `{name} = {"{"}`
 
 		local function AddToRet(addon)
-			ret = ret..`\n{addon}`
+			ret = ret .. `\n{addon}`
 		end
 
-		for i,v in pairs(table) do
+		for i, v in pairs(table) do
 			if type(v) == "table" then
-				AddToRet("	"..DecompTable(v,i))
+				AddToRet("	" .. DecompTable(v, i))
 			else
-				AddToRet("	"..DecompMain(v,i))
+				AddToRet("	" .. DecompMain(v, i))
 			end
 		end
 
-		return ret.."\n}"
+		return ret .. "\n}"
 	end
 
-	for i,v in req do
+	for i, v in req do
 		if typeof(v) == "table" then
-			AddToReturn(DecompTable(v,i))
+			AddToReturn(DecompTable(v, i))
 		else
-			AddToReturn(DecompMain(v,i))
+			AddToReturn(DecompMain(v, i))
 		end
 	end
 
-
-	return returnString..`\n{"return"} {mod.Name}`
+	return returnString .. `\n{"return"} {mod.Name}`
 end
 
 return ezDecomp
