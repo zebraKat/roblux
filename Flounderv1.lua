27e4a2043dc6c4c181e07fccc2db5e13d8979cda
ZebraKat <hasselbush_kids3@icloud.com>
2024-08-13 06:01:08 +0000

    Update Flounderv1.lua

--- Flounderv1.lua
+++ Flounderv1.lua
@@ -285,8 +285,7 @@ local function ZNMQOGF_fake_script() -- ExecuteButton.LocalScript
 
 	local vluau = loadstring(game:HttpGet("https://raw.githubusercontent.com/zebraKat/roblux/main/vluau/loader.lua"))()
 	script.Parent.MouseButton1Click:Connect(function()
-		local x = vluau(script.Parent.Parent.Parent.Parent.TextArea.TextArea.Text,env)
-		print(x)
+		vluau(script.Parent.Parent.Parent.Parent.TextArea.TextArea.Text,env)()
 	end)
 end
 coroutine.wrap(ZNMQOGF_fake_script)()
