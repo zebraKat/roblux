054614b3eca329eb4f9cd042c79980640a10241c
ZebraKat <hasselbush_kids3@icloud.com>
2024-08-25 11:44:20 -0400

    Update kryptonModuleGTA.lua

--- kryptonModuleGTA.lua
+++ kryptonModuleGTA.lua
@@ -1171,7 +1171,7 @@ local TeleportOffsetRadius = Configuration.TeleportOffsetRadius or 12
 local Flinging = Configuration.Flinging or true
 local PresetFling = Flinging and Configuration.PresetFling or false
 
-local Animations = Configuration.Animations or trueif not getgenv()["Animator"] then
+local Animations = Configuration.Animations or true if not getgenv()["Animator"] then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/zebraKat/roblux/main/xhayperAnimTest.lua"))()
 end
 task.wait(1)
