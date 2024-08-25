3691c106be04c362571300c3bc46c9d6ea34add9
ZebraKat <hasselbush_kids3@icloud.com>
2024-08-24 22:23:04 -0400

    Update kryptonfree.lua

--- kryptonfree.lua
+++ kryptonfree.lua
@@ -1,4 +1,7 @@
 local Hats = {
+    ["Head"] = {
+        {Mesh= "rbxassetid://17517530794", Texture = "rbxassetid://17517478791", Name = "Kleos Erebus", Offset = CFrame.Angles(0,0,0)}
+    },
     ["Right Arm"] = {
         {Texture = "4645402630", Mesh = "3030546036", Name = "International Fedora", Offset = CFrame.Angles(math.rad(-90), 0, math.rad(90))}
     },
@@ -17,22 +20,19 @@ local Hats = {
 
     ["Torso"] = {
         {Texture = "4819722776", Mesh = "4819720316", Name = "MeshPartAccessory", Offset = CFrame.Angles(0, 0, math.rad(-15))}
-    },
-    ["Head"] = {
-        {Mesh= "17517530794", Texture = "17517478791", Name = "Kleos Erebus", Offset = CFrame.Angles(0,0,0)}
     }
 }
 
 Configuration = {}
 Configuration.ReturnOnDeath = true
 Configuration.Flinging = true
-Configuration.PresetFling = false -- set to false if you gonna use the one from library
+Configuration.PresetFling = true -- set to false if you gonna use the one from library
 Configuration.Animations = true
-Configuration.WaitTime = 5
-Configuration.RigName = "FakeRig"
-Configuration.TeleportOffsetRadius = 1000
-Configuration.NoCollisions = false
-Configuration.AntiVoiding = false
+Configuration.WaitTime = 3
+Configuration.RigName = "sharklier"
+Configuration.TeleportOffsetRadius = 35
+Configuration.NoCollisions = true
+Configuration.AntiVoiding = true
 Configuration.SetSimulationRadius = true
 Configuration.DisableCharacterScripts = false
 Configuration.Hats = Hats
