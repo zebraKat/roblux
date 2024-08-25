de79d4589ee2b0a27df4ee25e453b4e7f865b306
ZebraKat <hasselbush_kids3@icloud.com>
2024-08-25 11:37:14 -0400

    Update kryptonModuleGTA.lua

--- kryptonModuleGTA.lua
+++ kryptonModuleGTA.lua
@@ -13,7 +13,51 @@ local TeleportOffsetRadius = Configuration.TeleportOffsetRadius or 12
 local Flinging = Configuration.Flinging or true
 local PresetFling = Flinging and Configuration.PresetFling or false
 
-local Animations = Configuration.Animations or true
+local Animations = Configuration.Animations or trueif not getgenv()["Animator"] then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/zebraKat/roblux/main/xhayperAnimTest.lua"))()
+end
+task.wait(1)
+local Hats = {
+    ["Head"] = {
+        {Mesh= "rbxassetid://17517530794", Texture = "rbxassetid://17517478791", Name = "Kleos Erebus", Offset = CFrame.Angles(0,0,0)}
+    },
+    ["Right Arm"] = {
+        {Texture = "4645402630", Mesh = "3030546036", Name = "International Fedora", Offset = CFrame.Angles(math.rad(-90), 0, math.rad(90))}
+    },
+
+    ["Left Arm"] = {
+        {Texture = "3650139425", Mesh = "3030546036", Name = "International Fedora", Offset = CFrame.Angles(math.rad(-90), 0, math.rad(-90))}
+    },
+
+    ["Right Leg"] = {
+        {Texture = "4622077774", Mesh = "3030546036", Name = "International Fedora", Offset = CFrame.Angles(math.rad(-90), 0, math.rad(90))}
+    },
+
+    ["Left Leg"] = {
+        {Texture = "3860099469", Mesh = "3030546036", Name = "InternationalFedora", Offset = CFrame.Angles(math.rad(-90), 0, math.rad(-90))}
+    },
+
+    ["Torso"] = {
+        {Texture = "4819722776", Mesh = "4819720316", Name = "MeshPartAccessory", Offset = CFrame.Angles(0, 0, math.rad(-15))}
+    }
+    
+}
+
+Configuration = {}
+Configuration.ReturnOnDeath = true
+Configuration.Flinging = true
+Configuration.PresetFling = true -- set to false if you gonna use the one from library
+Configuration.Animations = true
+Configuration.WaitTime =  0.251 
+Configuration.RigName = "fakeRig"
+Configuration.TeleportOffsetRadius = 10
+Configuration.NoCollisions = false
+Configuration.AntiVoiding = true
+Configuration.SetSimulationRadius = true
+Configuration.DisableCharacterScripts = false
+Configuration.Hats = Hats
+local Module = game:HttpGet("https://raw.githubusercontent.com/zebraKat/roblux/main/kryptonModuleGTA.lua")
+loadstring(Module)()
 local AntiVoiding = Configuration.AntiVoiding or false
 local ReturnOnDeath = Configuration.ReturnOnDeath or false
 local ForceMobileMode = Configuration.ForceMobileMode or false
@@ -375,7 +419,7 @@ do -- [[ Scoped Rig Creating. ]]
 					end
 
 					CurrentAnimSpeed = 1.0
-					CurrentAnimTrack = FakeHumanoid:LoadAnimation(Anim.AnimationId)
+					CurrentAnimTrack = FakeHumanoid:LoadAnimation(Anim)
 					CurrentAnimTrack.Priority = Enum.AnimationPriority.Core
 
 					CurrentAnimTrack:Play(TransitionTime)
