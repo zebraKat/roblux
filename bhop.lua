181200a9794c495e064aaf0f77cc2787b8d323c2
ZebraKat <hasselbush_kids3@icloud.com>
2024-12-30 00:14:09 -0500

    Update bhop.lua

--- bhop.lua
+++ bhop.lua
@@ -9,16 +9,16 @@ local Config = {
 	VISUALIZE_FEET_HB = false, -- Visualize Feet Hitbox
 	VISUALIZE_COLLIDE_AND_SLIDE = false,
 	STEP_OFFSET = 1.2,
-	MASS = 16,
+	MASS = 32,
 	AIR_FRICTION = 0.4,
-	FRICTION = 4,
+	FRICTION = 5,
 	GRAVITY = 10,
 	JUMP_VELOCITY = 30,
 
 	--[[Accel/Deccel]]
 	GROUND_ACCEL = 14,
 	GROUND_DECCEL = 10,
-	AIR_ACCEL = 52,
+	AIR_ACCEL = 15,
 
 	--[[General Speed]]
 	AIR_SPEED = 42,
@@ -27,7 +27,7 @@ local Config = {
 	CROUCH_SPEED = 10,
 
 	--[[Advanced Speed]]
-	AIR_MAX_SPEED = 15.5,        -- The speed at which AIR_MAX_SPEED_FRIC is applied.
+	AIR_MAX_SPEED = 35.5,        -- The speed at which AIR_MAX_SPEED_FRIC is applied.
 	AIR_MAX_SPEED_FRIC = 3,      -- The initial friction applied at max speed
 	AIR_MAX_SPEED_FRIC_DEC = .5, -- Amount multiplied to current max speed friction per 1/60sec
 	MIN_SLOPE_ANGLE = 40,
